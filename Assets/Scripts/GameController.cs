565f62072fa15ecef1c516eb8f6a14cf12ac3a96
Justin <justin.zachow@gmail.com>
2020-07-17 22:52:22 -0400

    Commit chapter 3 code. Add materials, prefabs, and basic behavior scripts

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject OneBallPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("AddBall", 1.5F, 1);
+    }
+
+    // Update is called once per frame
+    void AddBall()
+    {
+        Instantiate(OneBallPrefab);
+    }
+}
---
+++ Assets/Scripts/OneBallBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneBallBehaviour : MonoBehaviour
+{
+    static int ballCount = 0;
+    public float XRotation = 0;
+    public float YRotation = 1;
+    public float ZRotation = 0;
+    public float DegressPerSecond = 180;
+    public int BallNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(3 - Random.value * 6, 3 - Random.value * 6, 3 - Random.value * 6);
+        ballCount++;
+        ballNumber = ballCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 axis = new Vector3(XRotation, YRotation, ZRotation);
+        transform.RotateAround(Vector3.zero, axis, DegressPerSecond * Time.deltaTime);
+    }
+}
