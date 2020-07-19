2d413cc2e2bd50a1e1b3a7a19b9573c7432ea52f
Justin <justin.zachow@gmail.com>
2020-07-18 20:22:32 -0400

    Add Game UI and game mechanics logic

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public GameObject OneBallPrefab;
+    public int Score = 0;
+    public bool GameOver = true;
+    public int NumberOfBalls = 0;
+    public int MaximumBalls = 15;
+    public Text ScoreText;
+    public Button PlayAgainButton;
 
 
     // Start is called before the first frame update
@@ -16,6 +23,38 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void AddBall()
     {
-        Instantiate(OneBallPrefab);
+        if (!GameOver)
+        {
+            Instantiate(OneBallPrefab);
+            NumberOfBalls++;
+            if (NumberOfBalls >= MaximumBalls)
+            {
+                GameOver = true;
+                PlayAgainButton.gameObject.SetActive(true);
+            }
+        }        
+    }
+
+    public void ClickOnBall()
+    {
+        Score++;
+        NumberOfBalls--;
+    }
+
+    public void StartGame()
+    {
+        foreach (GameObject ball in GameObject.FindGameObjectsWithTag("GameController"))
+        {
+            Destroy(ball);
+        }
+        PlayAgainButton.gameObject.SetActive(false);
+        Score = 0;
+        NumberOfBalls = 0;
+        GameOver = false;
+    }
+
+    private void Update()
+    {
+        ScoreText.text = Score.ToString();
     }
 }
--- Assets/Scripts/OneBallBehaviour.cs
+++ Assets/Scripts/OneBallBehaviour.cs
@@ -5,24 +5,53 @@ using UnityEngine;
 public class OneBallBehaviour : MonoBehaviour
 {
     static int ballCount = 0;
-    public float XRotation = 0;
-    public float YRotation = 1;
-    public float ZRotation = 0;
-    public float DegressPerSecond = 180;
+    public float XSpeed;
+    public float YSpeed;
+    public float ZSpeed;
+    public float Multiplier = 0.75F;
     public int BallNumber;
+    public int TooFar = 5;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(3 - Random.value * 6, 3 - Random.value * 6, 3 - Random.value * 6);
+        ResetBall();
         ballCount++;
-        ballNumber = ballCount;
+        BallNumber = ballCount;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 axis = new Vector3(XRotation, YRotation, ZRotation);
-        transform.RotateAround(Vector3.zero, axis, DegressPerSecond * Time.deltaTime);
+        XSpeed += Multiplier - Random.value * Multiplier * 2;
+        YSpeed += Multiplier - Random.value * Multiplier * 2;
+        ZSpeed += Multiplier - Random.value * Multiplier * 2;
+        transform.Translate(Time.deltaTime * XSpeed, Time.deltaTime * YSpeed, Time.deltaTime * ZSpeed);
+
+        if ((Mathf.Abs(transform.position.x) > TooFar) || (Mathf.Abs(transform.position.y) > TooFar) || (Mathf.Abs(transform.position.z) > TooFar))
+        {
+            ResetBall();
+        }
+    }
+
+    void OnMouseDown()
+    {
+        GameController controller = Camera.main.GetComponent<GameController>();
+
+        if (!controller.GameOver)
+        {
+            controller.ClickOnBall();
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void ResetBall()
+    {
+        XSpeed = Multiplier - Random.value * Multiplier * 2;
+        YSpeed = Multiplier - Random.value * Multiplier * 2;
+        ZSpeed = Multiplier - Random.value * Multiplier * 2;
+        transform.position = new Vector3(3 - Random.value * 6, 3 - Random.value * 6, 3 - Random.value * 6);
     }
 }
